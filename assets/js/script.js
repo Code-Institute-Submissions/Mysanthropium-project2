3c30b58fac80924f950e0f428bf28ee5db2780c6
Rasmus Persson <rasmus.persson9@hotmail.com>
2023-04-18 09:30:32 +0000

    Add changes to all functions so they work without issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,96 +1,175 @@
-/**
- * Quiz function and forEach loop
- */
-function buildQuiz(){
-    const output = [];
-
-    myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-
-            const answers = [];
-
-            for(letter in currentQuestion.answers){
-
-                answers.push(
-                    `<label>
-                    <input type="radio" name="question${questionNumber}" value="${letter}">
-                    ${letter} : ${currentQuestion.answers[letter]}
-                    </label>`
-                );
-            }
-
-            output.push(
-                `<div class="question"> ${currentQuestion.question} </div>
-                <div class="answers"> ${answers.join('')} </div>`
-            );
-        }
-    );
-
-    quizContainer.innerHTML = output.join('');
-}
-/**
- * Results function with a forEach loop and results color accordingly
- */
-function showResults() {
-    const answerContainers = quizContainer.querySelectorAll('.answer');
-    let numCorrect = 0;
-    myQuestions.forEach( (currentQuestion, questionNumber) => {
-        /**
-         * find the selected answer in our HTML
-         */
-        const answerContainer = answerContainers[questionNumber];
-        const selector = `input[name=question${questionNumber}]:checked`;
-        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-        /**
-         * Using if/else statement to check if answer is correct or incorrect
-         */
-        if(userAnswer === currentQuestion.correctAnswer){
-            numCorrect++;
-            answerContainers[questionNumber].style.color = 'lightgreen';
-        } else{
-            answerContainers[questionNumber].style.color = 'red';
-        }
-    });
-
-    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-}
-
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-/**
- * Quiz questions
- */
-const myQuestions = [
+const questions = [
     {
         question: "How many band members had The Beatles?",
-        answers: {
-            a: "3",
-            b: "4",
-            c: "5"
-        },
-        correctAnswer: "b"
+        answers: [
+            { text: "3", correct: false},
+            { text: "4", correct: true},
+            { text: "5", correct: false},
+            { text: "6", correct: false},
+        ]
     },
     {
         question: "Who's the lead singer in the Foo Fighters?",
-        answers: {
-            a: "John Lennon",
-            b: "Ozzy Osbourne",
-            c: "Lemmy Kilmister",
-            d: "Dave Grohl"
-        },
-        correctAnswer: "d"
+        answers: [
+            { text: "John Lennon", correct: false},
+            { text: "Ozzy Osbourne", correct: false},
+            { text: "Lemmy Kilmister", correct: false},
+            { text: "Dave Grohl", correct: true},
+        ]
+    },
+    {
+        question: "What year did Lemmy Kilmister die?",
+        answers: [
+            { text: "2015", correct: true},
+            { text: "2017", correct: false},
+            { text: "2018", correct: false},
+            { text: "2020", correct: false},
+        ]
+    },
+    {
+        question: "What country is the band ABBA from?",
+        answers: [
+            { text: "England", correct: false},
+            { text: "Sweden", correct: true},
+            { text: "Denmark", correct: false},
+            { text: "Finland", correct: false},
+        ]
+    },
+    {
+        question: "What year did ABBA release their first album?",
+        answers: [
+            { text: "1972", correct: false},
+            { text: "1976", correct: false},
+            { text: "1983", correct: false},
+            { text: "1973", correct: true},
+        ]
+    },
+    {
+        question: "What year was the release of The Black Album by Metallica?",
+        answers: [
+            { text: "1991", correct: true},
+            { text: "1993", correct: false},
+            { text: "1987", correct: false},
+            { text: "1989", correct: false},
+        ]
+    },
+    {
+        question: "How many band members is there in Metallica?",
+        answers: [
+            { text: "3", correct: false},
+            { text: "5", correct: false},
+            { text: "4", correct: true},
+            { text: "6", correct: false},
+        ]
+    },
+    {
+        question: "What's Red Hot Chili Peppers basist called?",
+        answers: [
+            { text: "Mike", correct: false},
+            { text: "Flea", correct: true},
+            { text: "Balz", correct: false},
+            { text: "Michael", correct: false},
+        ]
+    },
+    {
+        question: "Who was the lead singer in Nirvana?",
+        answers: [
+            { text: "Dave Grohl", correct: false},
+            { text: "Chad Channing", correct: false},
+            { text: "Curt Cobain", correct: true},
+            { text: "Chester Bennington", correct: false},
+        ]
+    },
+    {
+        question: "What country is Metallicas drummer, Lars Ulrich from?",
+        answers: [
+            { text: "Sweden", correct: false},
+            { text: "Norway", correct: false},
+            { text: "Finland", correct: false},
+            { text: "Denmark", correct: true},
+        ]
     }
 ];
 
-/**
- * Start Quiz
- */
-buildQuiz();
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
 
-/**
- * submit button, show results
- */
-submitButton.addEventListener('click', showResults);
+let currentQuestionIndex = 0;
+let score = 0;
 
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
 
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+})
+
+startQuiz();
