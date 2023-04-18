096ba5c63298f8d1fe2800343bc337715e385a47
Rasmus Persson <rasmus.persson9@hotmail.com>
2023-04-18 07:30:52 +0000

    Add changes to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,130 +1,49 @@
-const questions = [
+/**
+ * Store elements in these variables
+ */
+const quizContainer = document.getElementById('quiz-box');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+function quizGame() {
+
+}
+
+function showResults() {
+
+}
+
+/**
+ * Display quiz
+ */
+quizGame();
+
+/**
+ * submit button, show results
+ */
+submitButton.addEventListener('click', showResults);
+
+/**
+ * Quiz questions
+ */
+const myQuestions = [
     {
         question: "How many band members had The Beatles?",
-        answers: [
-            { text: "3", correct: false},
-            { text: "4", correct: true},
-            { text: "5", correct: false},
-            { text: "6", correct: false},
-        ]
-    }, 
+        answers: {
+            a: "3",
+            b: "4",
+            c: "5"
+        },
+        correctAnswer: "b"
+    },
     {
         question: "Who's the lead singer in the Foo Fighters?",
-        answers: [
-            { text: "John Lennon", correct: false},
-            { text: "Ozzy Osbourne", correct: false},
-            { text: "Lemmy Kilmister", correct: false},
-            { text: "Dave Grohl", correct: true},
-        ]
-    },
-    {
-        question: "What year did Lemmy Kilmister die?",
-        answers: [
-            { text: "2015", correct: true},
-            { text: "2017", correct: false},
-            { text: "2018", correct: false},
-            { text: "2020", correct: false},
-        ]
-    },
-    {
-        question: "What country is the band ABBA from?",
-        answers: [
-            { text: "England", correct: false},
-            { text: "Sweden", correct: true},
-            { text: "Denmark", correct: false},
-            { text: "Finland", correct: false},
-        ]
-    },
-    {
-        question: "What year did ABBA release their first album?",
-        answers: [
-            { text: "1972", correct: false},
-            { text: "1976", correct: false},
-            { text: "1983", correct: false},
-            { text: "1973", correct: true},
-        ]
-    },
-    {
-        question: "What year was the release of The Black Album by Metallica?",
-        answers: [
-            { text: "1991", correct: true},
-            { text: "1993", correct: false},
-            { text: "1987", correct: false},
-            { text: "1989", correct: false},
-        ]
-    },
-    {
-        question: "How many band members is there in Metallica?",
-        answers: [
-            { text: "3", correct: false},
-            { text: "5", correct: false},
-            { text: "4", correct: true},
-            { text: "6", correct: false},
-        ]
-    },
-    {
-        question: "What's Red Hot Chili Peppers basist called?",
-        answers: [
-            { text: "Mike", correct: false},
-            { text: "Flea", correct: true},
-            { text: "Balz", correct: false},
-            { text: "Michael", correct: false},
-        ]
-    },
-    {
-        question: "Who was the lead singer in Nirvana?",
-        answers: [
-            { text: "Dave Grohl", correct: false},
-            { text: "Chad Channing", correct: false},
-            { text: "Curt Cobain", correct: true},
-            { text: "Chester Bennington", correct: false},
-        ]
-    },
-    {
-        question: "What country is Metallicas drummer, Lars Ulrich from?",
-        answers: [
-            { text: "Sweden", correct: false},
-            { text: "Norway", correct: false},
-            { text: "Finland", correct: false},
-            { text: "Denmark", correct: true},
-        ]
+        answers: {
+            a: "John Lennon",
+            b: "Ozzy Osbourne",
+            c: "Lemmy Kilmister",
+            d: "Dave Grohl"
+        },
+        correctAnswer: "d"
     }
-];
-
-const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answers");
-const nextButton = document.getElementById("next-question");
-
-let currentQuestionIndex = 0;
-let score = 0;
-
-function startQuiz() {
-    currentQuestionIndex = 0;
-    score = 0;
-    nextQuestion.innerHTML = "Next Question";
-    showQuestion();
-}
-
-function showQuestion() {
-    resetState();
-    let currentQuestion = questions[currentQuestionIndex];
-    let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.
-    question;
-
-    currentQuestion.answers.forEach(answer => {
-        const button = document.createElement("button");
-        button.innerHTML = answer.text;
-        button.classList.add("btn");
-        answerButtons.appendChild(button);
-    });
-}
-
-startQuiz();
-
-function resetState() {
-    nextButton.style.display = "none";
-    while(answerButtons.firstChild) {
-        answerButtons.removeChild(answerButtons.firstChild);
-    }
-}
+]
